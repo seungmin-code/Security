0c2c69174a9a804738b0c55ae55f1381b67dd913
YangSeungMin <ysmworkplace@gmail.com>
2025-02-03 11:11:18 +0900

    Initial commit

---
+++ src/test/java/com/min/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
